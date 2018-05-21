3edf8c86b54ffc76cd9c8abb3dc06bdbcfc724e8
Weslie <iweslie@vip.qq.com>
2018-05-21 21:27:17 +0800

    add ionic install scripts for mac

---
+++ mac_ionic_shell.sh
@@ -0,0 +1,7 @@
+brew install npm 
+alias npm="npm --registry=https://registry.npm.taobao.org \
+--disturl=https://npm.taobao.org/dist"
+
+source ~/.bash_profile
+
+npm install -g -ddd --save npm ionic cordova cordova-hot-code-push-cli jquery 
