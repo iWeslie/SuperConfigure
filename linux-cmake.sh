5e44e0236856b910e5469508485025be27fecbc5
Weslie <iweslie@vip.qq.com>
2018-11-05 16:58:34 +0800

    react native initialize steps and linux cmake shell

---
+++ linux-cmake.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+yum install tree 
+
+yum install -y git clang gcc-c++ uuid-devel libicu-devel icu libedit-devel libxml2-devel sqlite-devel swig python-devel ncurses-libs ncurses-devel pkgconfig libuuid-devel epel-release libbsd-devel 
+
+cd ~
+wget https://cmake.org/files/v3.9/cmake-3.9.0.tar.gz
+tar -zxvf cmake-3.9.0.tar.gz
+cd cmake-3.9.0
+./bootstrap --prefix=/usr
+make
+sudo make install
+
---
+++ npm-config.sh
@@ -0,0 +1,16 @@
+# https://reactnative.cn/docs/getting-started.html
+# React Native 中文网
+
+brew upgrade
+brew install node
+brew install watchman
+
+# 安装完 Node 后建议设置 npm 镜像以加速后面的过程
+npm config set registry https://registry.npm.taobao.org --global
+npm config set disturl https://npm.taobao.org/dist --global
+
+# Yarn是 Facebook 提供的替代 npm 的工具，可以加速 node 模块的下载。
+npm install -g yarn react-native-cli -ddd 
+# 安装完 yarn 后同理也要设置镜像源：
+yarn config set registry https://registry.npm.taobao.org --global
+yarn config set disturl https://npm.taobao.org/dist --global
