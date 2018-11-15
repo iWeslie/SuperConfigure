16976e1e8419f39c81dd04fec9579b5e4387967d
Weslie <iweslie@vip.qq.com>
2018-11-15 18:30:08 +0800

    add atom packages

--- atom_install.sh
+++ atom_install.sh
@@ -18,6 +18,20 @@ apm install minimap
 apm install autocomplete-paths
 apm install browser-refresh-on-save
 apm install open-in-browser
+apm install overscroll
+apm install goto-definition
+apm install react
+apm install atom-ternjs
+apm install atom-wrap-in-tag
+apm install autoclose-html
+apm install autocomplete-modules
+apm install color-picker
+apm install docblockr
+apm install emmet
+apm install emmet-jsx-css-modules
+apm install es6-javascript
+apm install language-babel
+
 
 kill `ps -ax | grep Atom | head -n1 | awk {'print$1'}`
 atom ~
--- npm-config.sh
+++ npm-config.sh
@@ -10,7 +10,9 @@ npm config set registry https://registry.npm.taobao.org --global
 npm config set disturl https://npm.taobao.org/dist --global
 
 # Yarn是 Facebook 提供的替代 npm 的工具，可以加速 node 模块的下载。
-npm install -g yarn react-native-cli -ddd 
+npm install -g yarn react-native-cli -ddd
 # 安装完 yarn 后同理也要设置镜像源：
 yarn config set registry https://registry.npm.taobao.org --global
 yarn config set disturl https://npm.taobao.org/dist --global
+
+npm i -D cowsay
